7f875806ee0d50fd3aa0f50e06a46c6d742e33cb
dinton harold <dintonharold@gmail.com>
2022-02-14 15:40:55 +0300

    Introduction to JSX and how it works, Adding a navbar using JSX

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Global variable called ReactDOM 
+// Global variable called ReactDOM
 // ReactDOM.render is used to display html
 // first parameter (what i want to render)
 // second parameter(where i want to render)
@@ -16,11 +16,8 @@ ReactDOM.render(
 2. It should return the content that we want to display on our page
 3. In the ReactDOM function, call the function you created like as if it's an html tag inorder to render it */
 
-function MainContent(){
-    return (
-        <h1>I'm learning React JS framework!</h1>
-        
-    )
+function MainContent() {
+  return <h1>I'm learning React JS framework!</h1>;
 }
 
 ReactDOM.render(
@@ -41,6 +38,31 @@ ReactDOM.render(
 append an h1 element the div with an id of root(without using innerHTML)
  */
 const h1 = document.createElement("h1");
-h1.textContent = "This is an imperative way to program"
-h1.className='header'
-document.getElementById("root").append(h1)
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1);
+
+/* JSX-Javascript XML 
+in JSX we use className instead of class
+When JSX is run, it returns objects that javascript can understand
+JSX renders a single parent element at a time, meaning if you want to render two paragraph elements at once, they should be wrapped in like a div*/
+const element = (
+  <div>
+    <h1 className="header">Hello, React!, I have to learn you by force</h1>
+    <p>Introducing JSX(Javascript XML)</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
+console.log(element);
+
+const navbar = (
+  <nav>
+    <h1>Dintaine SneakStore</h1>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+ReactDOM.render(navbar, document.getElementById("myNavBar"));
