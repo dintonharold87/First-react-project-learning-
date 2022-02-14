da1977e68a56e0c53c7523b285df60534b074ad3
dinton harold <dintonharold@gmail.com>
2022-02-14 12:07:49 +0300

    How to use react in your project using CDNS

---
+++ index.js
@@ -0,0 +1,12 @@
+// Global variable called ReactDOM 
+// ReactDOM.render is used to display html
+// first parameter (what i want to render)
+// second parameter(where i want to render)
+ReactDOM.render(<h1>Hello, React!, I have to learn you by force</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>wasap</li>
+    <li>folks</li>
+  </ul>,
+  document.getElementById("myList")
+);
